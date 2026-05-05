928b73128b2b842da4aed629d9ddf98dfea6cf0a
Yoaby23 <serveyourselfapp@gmail.com>
2026-05-04 22:59:06 -0600

    zzz

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-const supabaseUrl = 'https://spbgledcjwtqmlqdlpyx.supabase.co';
-const supabaseKey = 'sb_publishable_Uf9mS1Ev7fDYIld8PVCL5g_BwenGNXY';
-
-if (!window.supabase) {
-    window.supabase = supabaseOrigin.createClient(supabaseUrl, supabaseKey);
-}
