a4540389a9a51b21daaaa3ecf984b1a6087adc0d
Yoaby23 <serveyourselfapp@gmail.com>
2026-05-04 22:37:18 -0600

    c

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,7 @@
 const supabaseUrl = 'https://spbgledcjwtqmlqdlpyx.supabase.co';
 const supabaseKey = 'sb_publishable_Uf9mS1Ev7fDYIld8PVCL5g_BwenGNXY';
 
-// 2. Inicializar el cliente de Supabase
-const supabase = supabaseOrigin.createClient(supabaseUrl, supabaseKey);
-
-// Opcional: Verificar conexión en consola
-console.log("Conexión con Supabase establecida.");
+// Usamos window.supabase para evitar el error de "ya declarado"
+if (!window.supabase) {
+    window.supabase = supabaseOrigin.createClient(supabaseUrl, supabaseKey);
+}
