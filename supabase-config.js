009b2fd2cc754c37ef20b7bd8267fd9d15f7202c
Yoaby23 <serveyourselfapp@gmail.com>
2026-05-04 22:21:42 -0600

    s

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+// 1. Tus credenciales de Supabase (Sácalas de Project Settings > API)
+const supabaseUrl = 'https://spbgledcjwtqmlqdlpyx.supabase.co';
+const supabaseKey = 'sb_publishable_Uf9mS1Ev7fDYIld8PVCL5g_BwenGNXY';
+
+// 2. Inicializar el cliente de Supabase
+const supabase = supabaseOrigin.createClient(supabaseUrl, supabaseKey);
+
+// Opcional: Verificar conexión en consola
+console.log("Conexión con Supabase establecida.");
