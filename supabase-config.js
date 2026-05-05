11bca035dffacc349a7898ecf9478133fa7de88f
Yoaby23 <serveyourselfapp@gmail.com>
2026-05-04 22:39:06 -0600

    s

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,6 @@
 const supabaseUrl = 'https://spbgledcjwtqmlqdlpyx.supabase.co';
 const supabaseKey = 'sb_publishable_Uf9mS1Ev7fDYIld8PVCL5g_BwenGNXY';
 
-// Usamos window.supabase para evitar el error de "ya declarado"
 if (!window.supabase) {
     window.supabase = supabaseOrigin.createClient(supabaseUrl, supabaseKey);
 }
