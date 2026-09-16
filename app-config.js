31b6f8b926f3d0ce65d489b1ba89cda564419d11
Yoaby23 <serveyourselfapp@gmail.com>
2026-09-16 15:24:52 -0600

    Configure OneSignal and add notification opt-in

--- app-config.js
+++ app-config.js
@@ -1,5 +1,5 @@
 // Estas claves son públicas y deben restringirse por dominio en cada proveedor.
 // Las claves privadas se configuran únicamente como secretos de Supabase Functions.
 window.APP_CONFIG = {
-    oneSignalAppId: ''
+    oneSignalAppId: 'c77aa3bf-9fbf-4928-9441-18093c934dcd'
 };
